8e1ba6d8657d9295228526bef14c922ff8250c72
admiraladmirable <rick.morrow1204@gmail.com>
2025-04-19 16:58:43 -0500

    wip

---
+++ lua/kickstart/plugins/neo-tree.lua
@@ -0,0 +1,27 @@
+-- Neo-tree is a Neovim plugin to browse the file system
+-- https://github.com/nvim-neo-tree/neo-tree.nvim
+
+return {
+  'nvim-neo-tree/neo-tree.nvim',
+  -- NOTE: nixCats: return true only if category is enabled, else false
+  enabled = require('nixCatsUtils').enableForCategory("kickstart-neo-tree"),
+  version = '*',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
+  },
+  cmd = 'Neotree',
+  keys = {
+    { '<leader>e', ':Neotree reveal<CR>', { desc = 'NeoTree reveal' } },
+  },
+  opts = {
+    filesystem = {
+      window = {
+        mappings = {
+          ['<leader>e'] = 'close_window',
+        },
+      },
+    },
+  },
+}
