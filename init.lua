d015992d9f15b2475a15205c53aebbb3012c3c7e
admiraladmirable <rick.morrow1204@gmail.com>
2025-05-12 10:41:24 -0500

    test

--- init.lua
+++ init.lua
@@ -282,6 +282,21 @@ require('nixCatsUtils.lazyCat').setup(nixCats.pawsible { 'allPlugins', 'start', 
     'mrcjkb/rustaceanvim',
     version = '^6', -- Recommended
     lazy = false, -- This plugin is already lazy
+    config = function()
+      local bufnr = vim.api.nvim_get_current_buf()
+      vim.keymap.set('n', '<leader>ca', function()
+        vim.cmd.RustLsp 'codeAction' -- supports rust-analyzer's grouping
+        -- or vim.lsp.buf.codeAction() if you don't want grouping.
+      end, { silent = true, buffer = bufnr })
+      vim.keymap.set(
+        'n',
+        'K', -- Override Neovim's built-in hover keymap with rustaceanvim's hover actions
+        function()
+          vim.cmd.RustLsp { 'hover', 'actions' }
+        end,
+        { silent = true, buffer = bufnr }
+      )
+    end,
   },
 
   { -- LSP Configuration & Plugins
