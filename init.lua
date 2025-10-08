214c13afb3af2258c78271392368eebd552466c6
admiraladmirable <rick.morrow1204@gmail.com>
2025-10-08 12:12:29 -0500

    test trouble integration with telescope

--- init.lua
+++ init.lua
@@ -233,6 +233,7 @@ require('nixCatsUtils.lazyCat').setup(nixCats.pawsible { 'allPlugins', 'start', 
       },
       { 'nvim-telescope/telescope-ui-select.nvim' },
       { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
+      { 'folke/trouble.nvim' },
     },
     config = function()
       require('telescope').setup {
@@ -245,6 +246,23 @@ require('nixCatsUtils.lazyCat').setup(nixCats.pawsible { 'allPlugins', 'start', 
         extensions = {
           ['ui-select'] = {
             require('telescope.themes').get_dropdown(),
+          },
+        },
+      }
+
+      -- Trouble integration
+      local actions = require 'telescope.actions'
+      local open_with_trouble = require('trouble.sources.telescope').open
+
+      -- Use this to add more results without clearing the trouble list
+      local add_to_trouble = require('trouble.sources.telescope').add
+      local telescope = require 'telescope'
+
+      telescope.setup {
+        defaults = {
+          mappings = {
+            i = { ['<c-t>'] = open_with_trouble },
+            n = { ['<c-t>'] = open_with_trouble },
           },
         },
       }
