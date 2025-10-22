5444a6cec4f6e4931316233148f7d6d011185292
admiraladmirable <rick.morrow1204@gmail.com>
2025-10-22 12:25:37 -0500

    test

--- lua/myLuaConf/plugins/rust.lua
+++ lua/myLuaConf/plugins/rust.lua
@@ -17,9 +17,12 @@ return {
             vim.keymap.set('n', '<leader>rr', function()
               vim.cmd.RustLsp 'runnables'
             end, { buffer = bufnr, desc = 'Rust: Runnables' })
-            vim.keymap.set('n', '<leader>rh', function()
+            vim.keymap.set('n', 'K', function()
               vim.cmd.RustLsp { 'hover', 'actions' }
             end, { buffer = bufnr, desc = 'Rust: Hover Actions' })
+            vim.keymap.set('n', '<leader>ca', function()
+              vim.cmd.RustLsp 'codeAction'
+            end, { buffer = bufnr, desc = 'Rust: Code Actions' })
           end,
           default_settings = {
             ['rust-analyzer'] = {
