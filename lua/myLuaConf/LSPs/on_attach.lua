12ca49e2586e9990100a205129bc823b9d3f9e73
admiraladmirable <rick.morrow1204@gmail.com>
2025-10-22 12:48:05 -0500

    test

--- lua/myLuaConf/LSPs/on_attach.lua
+++ lua/myLuaConf/LSPs/on_attach.lua
@@ -11,7 +11,7 @@ return function(_, bufnr)
   end
 
   nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-  nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+  nmap('<leader>ca', vim.lsp.buf.code_action(), '[C]ode [A]ction')
 
   nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
 
