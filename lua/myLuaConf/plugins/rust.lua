40ab1c61f98d2c6a341ca29a3cbcdaea4dbda73a
admiraladmirable <rick.morrow1204@gmail.com>
2025-10-22 12:32:00 -0500

    ttesqtest

--- lua/myLuaConf/plugins/rust.lua
+++ lua/myLuaConf/plugins/rust.lua
@@ -7,22 +7,19 @@ return {
     for_cat = { cat = 'general', default = true },
     ft = { 'rust' },
     after = function()
-      vim.g.rustaceanvim = {
+      vim.g.rustaceanvim = vim.tbl_deep_extend('force', vim.g.rustaceanvim or {}, {
         tools = {
           hover_actions = { auto_focus = true },
         },
         server = {
           on_attach = function(client, bufnr)
             on_attach(client, bufnr)
-            -- vim.keymap.set('n', '<leader>rr', function()
-            --   vim.cmd.RustLsp 'runnables'
-            -- end, { buffer = bufnr, desc = 'Rust: Runnables' })
-            -- vim.keymap.set('n', 'K', function()
-            --   vim.cmd.RustLsp { 'hover', 'actions' }
-            -- end, { buffer = bufnr, desc = 'Rust: Hover Actions' })
-            -- vim.keymap.set('n', '<leader>ca', function()
-            --   vim.cmd.RustLsp 'codeAction'
-            -- end, { buffer = bufnr, desc = 'Rust: Code Actions' })
+            vim.keymap.set('n', '<leader>rr', function()
+              vim.cmd.RustLsp 'runnables'
+            end, { buffer = bufnr, desc = 'Rust: Runnables' })
+            vim.keymap.set('n', '<leader>rh', function()
+              vim.cmd.RustLsp { 'hover', 'actions' }
+            end, { buffer = bufnr, desc = 'Rust: Hover Actions' })
           end,
           default_settings = {
             ['rust-analyzer'] = {
