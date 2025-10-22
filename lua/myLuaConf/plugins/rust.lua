b04f5c72cc8642a190fe8e9787610a75344de3db
admiraladmirable <rick.morrow1204@gmail.com>
2025-10-22 12:53:45 -0500

    test

--- lua/myLuaConf/plugins/rust.lua
+++ lua/myLuaConf/plugins/rust.lua
@@ -6,33 +6,46 @@ return {
     version = '^6',
     for_cat = { cat = 'general', default = true },
     ft = { 'rust' },
-    after = function()
-      vim.g.rustaceanvim = {
-        tools = {
-          hover_actions = { auto_focus = true },
-        },
-        server = {
-          on_attach = function(client, bufnr)
-            on_attach(client, bufnr)
-            vim.keymap.set('n', '<leader>rr', function()
-              vim.cmd.RustLsp 'runnables'
-            end, { buffer = bufnr, desc = 'Rust: Runnables' })
-            vim.keymap.set('n', '<leader>rh', function()
-              vim.cmd.RustLsp { 'hover', 'actions' }
-            end, { buffer = bufnr, desc = 'Rust: Hover Actions' })
-          end,
-          default_settings = {
-            ['rust-analyzer'] = {
-              cargo = {
-                allFeatures = true,
-              },
-              check = {
-                command = 'clippy',
-              },
+    before = function()
+      local rustacean = vim.g.rustaceanvim or {}
+
+      rustacean.tools = vim.tbl_deep_extend('force', rustacean.tools or {}, {
+        hover_actions = { auto_focus = true },
+      })
+
+      local existing_on_attach = rustacean.server and rustacean.server.on_attach
+
+      local function rust_on_attach(client, bufnr)
+        on_attach(client, bufnr)
+
+        if existing_on_attach then
+          existing_on_attach(client, bufnr)
+        end
+
+        vim.keymap.set('n', '<leader>rr', function()
+          vim.cmd.RustLsp 'runnables'
+        end, { buffer = bufnr, desc = 'Rust: Runnables' })
+
+        vim.keymap.set('n', '<leader>rh', function()
+          vim.cmd.RustLsp { 'hover', 'actions' }
+        end, { buffer = bufnr, desc = 'Rust: Hover Actions' })
+      end
+
+      rustacean.server = vim.tbl_deep_extend('force', rustacean.server or {}, {
+        on_attach = rust_on_attach,
+        default_settings = {
+          ['rust-analyzer'] = {
+            cargo = {
+              allFeatures = true,
+            },
+            check = {
+              command = 'clippy',
             },
           },
         },
-      }
+      })
+
+      vim.g.rustaceanvim = rustacean
     end,
   },
 }
