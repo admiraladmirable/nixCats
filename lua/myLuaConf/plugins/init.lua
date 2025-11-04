7f9c5f502afb9586dcdacecf4f1ffc9040ade5d8
admiraladmirable <rick.morrow1204@gmail.com>
2025-11-04 15:46:25 -0600

    test

--- lua/myLuaConf/plugins/init.lua
+++ lua/myLuaConf/plugins/init.lua
@@ -200,7 +200,23 @@ require('lze').load {
         notifier = { enabled = true },
         quickfile = { enabled = true },
         scope = { enabled = true },
-        scroll = { enabled = true },
+        scroll = {
+          enabled = true,
+          animate = {
+            duration = { step = 5, total = 50 },
+            easing = 'linear',
+          },
+          -- faster animation when repeating scroll after delay
+          animate_repeat = {
+            delay = 100, -- delay in ms before using the repeat animation
+            duration = { step = 5, total = 5 },
+            easing = 'linear',
+          },
+          -- what buffers to animate
+          filter = function(buf)
+            return vim.g.snacks_scroll ~= false and vim.b[buf].snacks_scroll ~= false and vim.bo[buf].buftype ~= 'terminal'
+          end,
+        },
         statuscolumn = { enabled = true },
         words = { enabled = true },
       }
