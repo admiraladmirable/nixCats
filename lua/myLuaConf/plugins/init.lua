a2b53feefad8022e98efa96d8ef990eb33df2b7b
admiraladmirable <rick.morrow1204@gmail.com>
2026-02-02 10:41:54 -0600

    test in after function

--- lua/myLuaConf/plugins/init.lua
+++ lua/myLuaConf/plugins/init.lua
@@ -162,44 +162,9 @@ require('lze').load {
     for_cat = 'general.extra',
     priority = 1000,
     lazy = false,
-    init = function()
-      vim.api.nvim_create_autocmd('User', {
-        pattern = 'VeryLazy',
-        callback = function()
-          _G.dd = function(...)
-            Snacks.debug.inspect(...)
-          end
-          _G.bt = function()
-            Snacks.debug.backtrace()
-          end
-
-          if vim.fn.has 'nvim-0.11' == 1 then
-            vim._print = function(_, ...)
-              dd(...)
-            end
-          else
-            vim.print = _G.dd
-          end
-
-          Snacks.toggle.option('spell', { name = 'Spelling' }):map('<leader>us')
-          Snacks.toggle.option('wrap', { name = 'Wrap' }):map('<leader>uw')
-          Snacks.toggle.option('relativenumber', { name = 'Relative Number' }):map('<leader>uL')
-          Snacks.toggle.diagnostics():map('<leader>ud')
-          Snacks.toggle.line_number():map('<leader>ul')
-          Snacks.toggle.option('conceallevel', {
-            off = 0,
-            on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2,
-          }):map('<leader>uc')
-          Snacks.toggle.treesitter():map('<leader>uT')
-          Snacks.toggle.option('background', { off = 'light', on = 'dark', name = 'Dark Background' }):map('<leader>ub')
-          Snacks.toggle.inlay_hints():map('<leader>uh')
-          Snacks.toggle.indent():map('<leader>ug')
-          Snacks.toggle.dim():map('<leader>uD')
-        end,
-      })
-    end,
     after = function(_)
-      require('snacks').setup {
+      local snacks = require 'snacks'
+      snacks.setup {
         bigfile = { enabled = true },
         terminal = { enabled = true },
         debug = { enabled = true },
@@ -263,6 +228,36 @@ require('lze').load {
         toggle = { enabled = true },
         words = { enabled = true },
       }
+
+      _G.dd = function(...)
+        snacks.debug.inspect(...)
+      end
+      _G.bt = function()
+        snacks.debug.backtrace()
+      end
+
+      if vim.fn.has 'nvim-0.11' == 1 then
+        vim._print = function(_, ...)
+          dd(...)
+        end
+      else
+        vim.print = _G.dd
+      end
+
+      snacks.toggle.option('spell', { name = 'Spelling' }):map('<leader>us')
+      snacks.toggle.option('wrap', { name = 'Wrap' }):map('<leader>uw')
+      snacks.toggle.option('relativenumber', { name = 'Relative Number' }):map('<leader>uL')
+      snacks.toggle.diagnostics():map('<leader>ud')
+      snacks.toggle.line_number():map('<leader>ul')
+      snacks.toggle.option('conceallevel', {
+        off = 0,
+        on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2,
+      }):map('<leader>uc')
+      snacks.toggle.treesitter():map('<leader>uT')
+      snacks.toggle.option('background', { off = 'light', on = 'dark', name = 'Dark Background' }):map('<leader>ub')
+      snacks.toggle.inlay_hints():map('<leader>uh')
+      snacks.toggle.indent():map('<leader>ug')
+      snacks.toggle.dim():map('<leader>uD')
     end,
     keys = {
       -- Top Pickers & Explorer
