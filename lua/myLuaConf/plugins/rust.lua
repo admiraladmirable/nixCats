4e0b52d43dceccae0e1eae6b91179d59f2bca2c6
admiraladmirable <rick.morrow1204@gmail.com>
2026-02-02 10:49:49 -0600

    test different hints for rustaceanvim

--- lua/myLuaConf/plugins/rust.lua
+++ lua/myLuaConf/plugins/rust.lua
@@ -19,37 +19,49 @@ return {
                 command = 'clippy',
               },
               inlayHints = {
-                bindingModeHints = {
-                  enable = false,
-                },
-                chainingHints = {
-                  enable = true,
-                },
-                closingBraceHints = {
-                  enable = true,
-                  minLines = 25,
-                },
-                closureReturnTypeHints = {
-                  enable = 'never',
-                },
-                lifetimeElisionHints = {
-                  enable = 'never',
-                  useParameterNames = false,
-                },
-                maxLength = 25,
-                parameterHints = {
-                  enable = true,
-                },
-                reborrowHints = {
-                  enable = 'never',
-                },
+                typeHints = { enable = true },
+                parameterHints = { enable = true },
+                bindingModeHints = { enable = false },
+                chainingHints = { enable = true },
+                closingBraceHints = { enable = true, minLines = 25 },
+                closureReturnTypeHints = { enable = 'never' },
+                lifetimeElisionHints = { enable = 'skip_trivial' },
+                reborrowHints = { enable = 'never' },
                 renderColons = true,
-                typeHints = {
-                  enable = true,
-                  hideClosureInitialization = false,
-                  hideNamedConstructor = false,
-                },
+                maxLength = 30,
               },
+              -- inlayHints = {
+              --   bindingModeHints = {
+              --     enable = false,
+              --   },
+              --   chainingHints = {
+              --     enable = true,
+              --   },
+              --   closingBraceHints = {
+              --     enable = true,
+              --     minLines = 25,
+              --   },
+              --   closureReturnTypeHints = {
+              --     enable = 'never',
+              --   },
+              --   lifetimeElisionHints = {
+              --     enable = 'never',
+              --     useParameterNames = false,
+              --   },
+              --   maxLength = 25,
+              --   parameterHints = {
+              --     enable = true,
+              --   },
+              --   reborrowHints = {
+              --     enable = 'never',
+              --   },
+              --   renderColons = true,
+              --   typeHints = {
+              --     enable = true,
+              --     hideClosureInitialization = false,
+              --     hideNamedConstructor = false,
+              --   },
+              -- },
             },
           },
         },
